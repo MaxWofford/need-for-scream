685d7ba158bbbbefabd1120b40b5593e47330ee9
Max Wofford <max@maxwofford.com>
2016-06-10 20:19:39 -0700

    Add volume detection

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
+var volumeArray = [];
 var fb = new Firebase("https://need-for-scream.firebaseio.com/");
 
-function setup() {
+var vol = function() {
+  // Returns a smoothed microphone volume
+  if (volumeArray.push(mic.getLevel()) > 20) {
+    volumeArray.splice(0, 1);
+  }
+  var sum = 0;
+  for (var i = 0; i < volumeArray.length; i++) {
+    sum += volumeArray[i];
+  }
+  return sum / volumeArray.length;
+}
 
+function setup() {
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
