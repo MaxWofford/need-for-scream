c64f4a39b59b6a77c89a492a8a0bb06e45a90427
Max Wofford <max@maxwofford.com>
2016-06-11 00:40:10 -0700

    Add voice subtitles

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var volume, player;
+var volume, player, recognizing;
 var volumeArray = [];
 var fb = new Firebase("https://need-for-scream.firebaseio.com/");
 
@@ -14,15 +14,19 @@ var getVolume = function() {
   return sum / volumeArray.length;
 }
 
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
 function preload() {
   playerImage = loadImage("/img/player.png");
 }
 
 function setup() {
-  createCanvas(500, 300);
+  createCanvas(windowWidth, windowHeight);
   mic = new p5.AudioIn();
   mic.start();
-  player = createSprite(width - 50, height - 50, 10, 5);
+  player = createSprite(50, 50, 10, 5);
   player.addImage(playerImage);
   player.rotateToDirection = true;
   player.maxSpeed = 6;
@@ -45,141 +49,39 @@ function draw() {
   drawSprites();
 }
 
-showInfo('info_start');
+//---------------------------
+var recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = true;
+recognition.onend = reset;
 
+recognition.onresult = function (event) {
+  var interim_transcript = '';
+  var final_transcript = '';
 
-
-var final_transcript = '';
-var recognizing = false;
-var ignore_onend;
-var start_timestamp;
-if (!('webkitSpeechRecognition' in window)) {
-  upgrade();
-} else {
-  start_button.style.display = 'inline-block';
-  var recognition = new webkitSpeechRecognition();
-  recognition.continuous = true;
-  recognition.interimResults = true;
-
-  recognition.onstart = function() {
-    recognizing = true;
-    showInfo('info_speak_now');
-    start_img.src = 'mic-animate.gif';
-  };
-
-  recognition.onerror = function(event) {
-    if (event.error == 'no-speech') {
-      start_img.src = 'mic.gif';
-      showInfo('info_no_speech');
-      ignore_onend = true;
+  for (var i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+      final_transcript += event.results[i][0].transcript;
+    } else {
+      interim_transcript += event.results[i][0].transcript;
     }
-    if (event.error == 'audio-capture') {
-      start_img.src = 'mic.gif';
-      showInfo('info_no_microphone');
-      ignore_onend = true;
-    }
-    if (event.error == 'not-allowed') {
-      if (event.timeStamp - start_timestamp < 100) {
-        showInfo('info_blocked');
-      } else {
-        showInfo('info_denied');
-      }
-      ignore_onend = true;
-    }
-  };
-
-  recognition.onend = function() {
-    recognizing = false;
-    if (ignore_onend) {
-      return;
-    }
-    start_img.src = 'mic.gif';
-    if (!final_transcript) {
-      showInfo('info_start');
-      return;
-    }
-    showInfo('');
-    if (window.getSelection) {
-      window.getSelection().removeAllRanges();
-      var range = document.createRange();
-      range.selectNode(document.getElementById('final_span'));
-      window.getSelection().addRange(range);
-    }
-
-  };
-
-  recognition.onresult = function(event) {
-    var interim_transcript = '';
-    for (var i = event.resultIndex; i < event.results.length; ++i) {
-      if (event.results[i].isFinal) {
-        final_transcript += event.results[i][0].transcript;
-      } else {
-        interim_transcript += event.results[i][0].transcript;
-      }
-    }
-    if (final_transcript.match(/forward/)) {
-      player.move.forward(getVolume());
-    }
-    if (final_transcript.match(/right/)) {
-      player.move.right(getVolume());
-    }
-    if (final_transcript.match(/left/)) {
-      player.move.left(getVolume());
-    }
-    final_span.innerHTML = linebreak(final_transcript);
-    interim_span.innerHTML = linebreak(interim_transcript);
-    if (final_transcript || interim_transcript) {
-      showButtons('inline-block');
-    }
-  };
-}
-
-
-
-var two_line = /\n\n/g;
-var one_line = /\n/g;
-function linebreak(s) {
-  return s.replace(two_line, '<p></p>').replace(one_line, '<br>');
-}
-
-var first_char = /\S/;
-
-
-
-
-function startButton(event) {
-  if (recognizing) {
-    recognition.stop();
-    return;
   }
-  final_transcript = '';
+  console.log('f',final_transcript);
+  console.log('i',interim_transcript);
+  results = interim_transcript.split(' ')
+  resultDiv = document.querySelector('#recognized-text')
+  message = results[results.length - 1]
+  if (message != '') {
+    resultDiv.textContent = message
+    resultDiv.style.fontSize = 200 / message.length + 'vw'
+    console.log(resultDiv.style.width)
+  }
+};
+
+function reset() {
   recognition.start();
-  ignore_onend = false;
-  final_span.innerHTML = '';
-  interim_span.innerHTML = '';
-  start_img.src = 'mic-slash.gif';
-  showInfo('info_allow');
-  showButtons('none');
-  start_timestamp = event.timeStamp;
 }
 
-function showInfo(s) {
-  if (s) {
-    for (var child = info.firstChild; child; child = child.nextSibling) {
-      if (child.style) {
-        child.style.display = child.id == s ? 'inline' : 'none';
-      }
-    }
-    info.style.visibility = 'visible';
-  } else {
-    info.style.visibility = 'hidden';
-  }
-}
+recognition.start();
+// ----------------------------
 
-var current_style;
-function showButtons(style) {
-  if (style == current_style) {
-    return;
-  }
-  current_style = style;
-}
